8d18852f9cb524e34af30774a645fbae32ac6b44
omarmiatello <jackl85@gmail.com>
2019-01-29 15:32:24 +0100

    Refactor coroutine. Add CoroutineScope

--- src/main/kotlin/Pizza.kt
+++ src/main/kotlin/Pizza.kt
@@ -1,5 +1,9 @@
 package com.github.jacklt.hashcode
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.io.File
 import kotlin.system.measureTimeMillis
@@ -9,38 +13,45 @@ const val VISUALIZE = false
 
 fun main() {
     measureTimeMillis {
-        val fn = listOf("example", "small", "medium", "big")
-        fn.slice(0..3).forEach {
-            App.solveForX(inputFile = File("io/$it.in"), outputFile = File("io/$it.out"))
+        runBlocking {
+            val fn = listOf("example", "small", "medium", "big")
+            fn.slice(0..3).forEach {
+                App.solveForX(inputFile = File("io/$it.in"), outputFile = File("io/$it.out")).join()
+            }
         }
     }.also { println("Completed in ${it}ms") }
 }
 
 data class Problem(
-        val rows: Int,  // R (1≤R≤1000)  isthenumberofrows,
-        val columns: Int,  // C (1 ≤ C ≤ 1000) is the number of columns,
-        val minIng: Int,  // L (1 ≤ L ≤ 1000)  is the minimum number of each ingredient cells in a slice,
-        val maxSliceSize: Int,  // H (1 ≤ H ≤ 1000)  is the maximum total number of cells of a slice
-        val mashrooms: List<List<Boolean>>
+    val rows: Int,  // R (1≤R≤1000)  isthenumberofrows,
+    val columns: Int,  // C (1 ≤ C ≤ 1000) is the number of columns,
+    val minIng: Int,  // L (1 ≤ L ≤ 1000)  is the minimum number of each ingredient cells in a slice,
+    val maxSliceSize: Int,  // H (1 ≤ H ≤ 1000)  is the maximum total number of cells of a slice
+    val mashrooms: List<List<Boolean>>
 )
 
 data class Slice(var x1: Int, var y1: Int, var x2: Int, var y2: Int)
 data class Solution(val slices: List<Slice>)
 
-object App {
-    fun solveForX(inputFile: File, outputFile: File) = runBlocking {
+object App : CoroutineScope {
+    val job = Job()
+    override val coroutineContext = job
+
+    fun solveForX(inputFile: File, outputFile: File) = launch {
         val problem = parse(inputFile)
         if (DEBUG) println("Problem: $problem")
 
         // do some stuff
 
-        val solution = Solution(listOf(
-            Slice(0, 0, 2, 1),
-            Slice(0, 2, 2, 2),
-            Slice(0, 3, 2, 4)
-        ))
+        val solution = async {
+            Solution(listOf(
+                Slice(0, 0, 2, 1),
+                Slice(0, 2, 2, 2),
+                Slice(0, 3, 2, 4)
+            ))
+        }
 
-        if (DEBUG) println("Solution: $solution")
+        if (DEBUG) println("Solution: ${solution.await()}()")
         // write(outputFile, solution)
     }
 }
